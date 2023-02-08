6f12ead1372b3a4699ed6b293b544b1c9c45567b
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2023-02-08 09:40:17 +0100

    Created host for images.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import image1 from "./assets/Numer 1.jpg";
+import image2 from "./assets/Numer 2.jpg";
+import image3 from "./assets/Numer 3.jpg";
+import image4 from "./assets/Numer 4.jpg";
+import image5 from "./assets/Numer 5.jpg";
+import image6 from "./assets/Numer 6.jpg";
+import image7 from "./assets/Numer 7.jpg";
+import image8 from "./assets/Numer 8.jpg";
+import image9 from "./assets/Numer 9.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div><img src={image1} alt="Świeczka z wosku" /></div>
+      <div><img src={image2} alt="Świeczka z wosku" /></div>
+      <div><img src={image3} alt="Świeczka z wosku" /></div>
+      <div><img src={image4} alt="Świeczka z wosku" /></div>
+      <div><img src={image5} alt="Świeczka z wosku" /></div>
+      <div><img src={image6} alt="Świeczka z wosku" /></div>
+      <div><img src={image7} alt="Świeczka z wosku" /></div>
+      <div><img src={image8} alt="Świeczka z wosku" /></div>
+      <div><img src={image9} alt="Świeczka z wosku" /></div>
     </div>
   );
 }
